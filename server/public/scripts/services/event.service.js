ccccd5ee105ff9a0eda8980993b8cdf749c41b8b
Chase Whitney <ChaseEWhitney@gmail.com>
2017-09-15 08:40:53 -0500

    code comments

--- server/public/scripts/services/event.service.js
+++ server/public/scripts/services/event.service.js
@@ -1,375 +1,327 @@
 myApp.factory('EventService', function($http, $location, UserService, NgMap){
   console.log('Event Loaded');
 
-var ev = {};
+  var ev = {};
 
-NgMap.getMap().then(function(map) {
+  NgMap.getMap().then(function(map) {
     console.log('map', map);
     ev.map = map;
   });
-
-
+  
   ev.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-        'WY').split(' ').map(function(state) {
-            return {abbrev: state};
-          });
-ev.userToView = {};
-ev.newEvent = {};
-ev.newEvent.games = [];
-ev.eventToEdit = {};
-ev.eventArray = [];
-ev.userService = UserService;
-ev.userObject = UserService.userObject;
-ev.eventToView = {};
-ev.myEvents =[];
-ev.myRealEvents = {
-  admin: [],
-  attending: [],
-  pending: [],
-  saved: [],
-  denied: [],
-};
-ev.searchParams={};
-ev.searchParams.games=[];
-ev.event = ev.eventArray[0];
+  'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+  'WY').split(' ').map(function(state) {
+    return {abbrev: state};
+  });
+  ev.userToView = {};
+  ev.newEvent = {};
+  ev.newEvent.games = [];
+  ev.eventToEdit = {};
+  ev.eventArray = [];
+  ev.userService = UserService;
+  ev.userObject = UserService.userObject;
+  ev.eventToView = {};
+  ev.myEvents =[];
+  ev.myRealEvents = {
+    admin: [],
+    attending: [],
+    pending: [],
+    saved: [],
+    denied: [],
+  };
+  ev.searchParams={};
+  ev.searchParams.games=[];
+  ev.event = ev.eventArray[0];
 
-ev.getEvents = function(){
-  $http.get('/event/search').then(function(response){
+  // Gets events to display on map
+  ev.getEvents = function(){
+    $http.get('/event/search').then(function(response){
       console.log('mongooseTest GET response:', response);
       ev.eventArray = response.data;
       console.log('eventArray is: ', ev.eventArray);
-
     });
-};
+  };
 
-ev.filteredSearch = function(){
-  console.log('ev.searchParams is:', ev.searchParams);
-  var searchArray = [];
-  if(ev.searchParams.game){
-    searchArray.push({ games: ev.searchParams.game});
-  }
-  if(ev.searchParams.skill && ev.searchParams.skill != "Any"){
-    searchArray.push({ skill: ev.searchParams.skill});
-  }
-  if(ev.searchParams.type && ev.searchParams.type != "Any"){
-    searchArray.push({ type: ev.searchParams.type});
-  }
-  console.log('searchArray is:', searchArray);
+  // Filters searched events by event type, skill level, and/or game
+  ev.filteredSearch = function(){
+    console.log('ev.searchParams is:', ev.searchParams);
+    var searchArray = [];
+    if(ev.searchParams.game){
+      searchArray.push({ games: ev.searchParams.game});
+    }
+    if(ev.searchParams.skill && ev.searchParams.skill != "Any"){
+      searchArray.push({ skill: ev.searchParams.skill});
+    }
+    if(ev.searchParams.type && ev.searchParams.type != "Any"){
+      searchArray.push({ type: ev.searchParams.type});
+    }
+    console.log('searchArray is:', searchArray);
+    if(searchArray.length > 0){
+      $http.put('/event/filteredsearch/', searchArray).then(function(response){
+        console.log('received response from filteredsearch PUT');
+        ev.eventArray = response.data;
+        console.log('eventArray is:', ev.eventArray);
+      });
+    } else {
+      ev.getEvents();
+    }
+  };
 
-  if(searchArray.length > 0){
-  $http.put('/event/filteredsearch/', searchArray).then(function(response){
-    console.log('received response from filteredsearch PUT');
-    ev.eventArray = response.data;
-    console.log('eventArray is:', ev.eventArray);
-
-  });
-} else {
-  ev.getEvents();
-}
-};
-
-
-
-ev.client = filestack.init('Asuq5FpJHThCT68CMbHISz');
-ev.returnPicker = function() {
-  var newImg = [];
+  // Filestack upload image
+  ev.client = filestack.init('Asuq5FpJHThCT68CMbHISz');
+  ev.returnPicker = function() {
+    var newImg = [];
     ev.client.pick({
     }).then(function(result) {
-        console.log(JSON.stringify(result.filesUploaded));
-        console.log(result.filesUploaded[0].url);
-
-        console.log('type:', typeof newImg);
-        newImg[0] = "https://cdn.filestackcontent.com/";
-        var temp = result.filesUploaded[0].handle;
-        newImg[1] = temp;
-        console.log('img:', newImg);
-        console.log('type:', typeof newImg);
-        console.log('0:', newImg[0]);
-        console.log('1:', newImg[1]);
-        console.log('type 0:', typeof newImg[0]);
-        console.log('type 1:', typeof newImg[1]);
-
+      console.log(JSON.stringify(result.filesUploaded));
+      console.log(result.filesUploaded[0].url);
+      newImg[0] = "https://cdn.filestackcontent.com/";
+      var temp = result.filesUploaded[0].handle;
+      newImg[1] = temp;
     });
     return newImg;
-};
+  };
 
-
-ev.getMarkerIcon = function(type){
-  switch(type){
-    case "board":
+  // Sets which icon will be displayed on map - grill, dice, or controller
+  ev.getMarkerIcon = function(type){
+    switch(type){
+      case "board":
       return "https://cdn.filestackcontent.com/wB4xspHjRbleGDmjOqQq";
-    case "outdoor":
+      case "outdoor":
       return "https://cdn.filestackcontent.com/Qi4VhBLTZzKUm3Qx9Mug";
-    default:
+      default:
       return "https://cdn.filestackcontent.com/qI8yPkrZTr2mOqx26xmh";
-  }
-};
+    }
+  };
 
-ev.showDetail = function(e, event) {
-  ev.event = event;
-  console.log('event in showDetail is: ', event);
-  console.log('event._id in showDetail is: ', event._id);
-  ev.map.showInfoWindow('foo-iw', event._id);
-};
+  // Shows details for selected event
+  ev.showDetail = function(e, event) {
+    ev.event = event;
+    console.log('event in showDetail is: ', event);
+    console.log('event._id in showDetail is: ', event._id);
+    ev.map.showInfoWindow('foo-iw', event._id);
+  };
 
-ev.hideDetail = function() {
-  ev.map.hideInfoWindow('foo-iw');
-};
+  // Hides details for selected event
+  ev.hideDetail = function() {
+    ev.map.hideInfoWindow('foo-iw');
+  };
 
-ev.updateEvent = function(){
-  console.log('in updateEvent, sending ev.eventToView: ', ev.eventToView);
+  // Updates event info
+  ev.updateEvent = function(){
+    console.log('in updateEvent, sending ev.eventToView: ', ev.eventToView);
+    swal(
+      'Event Updated!',
+      '',
+      'success'
+    );
+    $http.put('/event/updateevent', ev.eventToView).then(function(response){
+      console.log('got response from updateEvent PUT');
+    });
+  };
 
-  swal(
-    'Event Updated!',
-    '',
-    'success'
-  );
+  // Links to page to view selected event in detail
+  ev.goToEvent = function(viewEvent){
+    console.log('goToEvent event is:', viewEvent);
+    ev.eventToView = viewEvent;
+    $location.path('/viewevent');
+  };
 
-  $http.put('/event/updateevent', ev.eventToView).then(function(response){
-    console.log('got response from updateEvent PUT');
-  });
-};
+  // Links to page to edit an event
+  ev.goToEditEvent = function(viewEvent){
+    console.log('goToEditEvent event is:', viewEvent);
+    ev.eventToView = viewEvent;
+    $location.path('/editmyevent');
+  };
 
-ev.goToEvent = function(viewEvent){
-  console.log('goToEvent event is:', viewEvent);
-  ev.eventToView = viewEvent;
-  $location.path('/viewevent');
-};
-
-ev.goToEditEvent = function(viewEvent){
-  console.log('goToEditEvent event is:', viewEvent);
-  ev.eventToView = viewEvent;
-  $location.path('/editmyevent');
-};
-
-// add event to saved events
-ev.addToSaved = function(){
-  var eid = ev.eventToView._id;
-  console.log('id in addToSaved is:', eid);
-  console.log('id :', typeof eid);
-  $http.put('/event/addtosaved/' + eid).then(function(response){
+  // Add event to saved events
+  ev.addToSaved = function(){
+    var eid = ev.eventToView._id;
+    console.log('id in addToSaved is:', eid);
+    console.log('id :', typeof eid);
+    $http.put('/event/addtosaved/' + eid).then(function(response){
       console.log('got response from addtosaved PUT');
       ev.eventToView.saved.push(ev.userObject.userName);
     });
-
-};
-
-
-// get all events associated with user
-ev.getMyEvents = function(){
-    $http.get('/event/myevents').then(function(response){
-        console.log('getMyEvents GET response.data:', response.data);
-        ev.myEvents = response.data;
-        console.log('ev.myEvents is:', ev.myEvents);
-        // for(var i = 0; i < ev.myEvents.length; i++){
-        //   if(ev.myEvents[i].admin.includes(ev.userObject.username)) {
-        //
-        //   }
-        //
-        // }
-
-
-      });
   };
 
-// // save event //// UNUSED?
-// ev.saveEvent = function(id){
-//   $http.put('/event/save/' + id).then(function(response){
-//       console.log('got response from addtosaved PUT');
-//     });
-// };
 
-// request to attend the event that is being viewed
+  // Get all events associated with user
+  ev.getMyEvents = function(){
+    $http.get('/event/myevents').then(function(response){
+      console.log('getMyEvents GET response.data:', response.data);
+      ev.myEvents = response.data;
+      console.log('ev.myEvents is:', ev.myEvents);
+    });
+  };
 
-ev.requestToAttend = function(id){
-  console.log('requesting to Attend id:', id);
-  console.log('ev.eventToView is:', ev.eventToView);
-
-  swal(
-    "Now it's up to the host!",
-    "(We'll put in a good word.)",
-    'success'
-  );
-
-  $http.put('/event/requestattend/' + id).then(function(response){
+  // Request to attend an event
+  ev.requestToAttend = function(id){
+    console.log('requesting to Attend id:', id);
+    console.log('ev.eventToView is:', ev.eventToView);
+    swal(
+      "Now it's up to the host!",
+      "(We'll put in a good word.)",
+      'success'
+    );
+    $http.put('/event/requestattend/' + id).then(function(response){
       console.log('got response from addtosaved PUT');
     });
     ev.eventToView.pending.push(ev.userObject.userName);
     console.log('ev.eventToView is:', ev.eventToView);
     ev.getMyEvents();
-};
+  };
 
-
-
-
-
-
-// admin approves pending request
-ev.approveRequest = function(event, requester){
-  console.log('in approveRequest');
-  console.log('requester is:',requester);
-  console.log('event is:', event);
-  var data = {};
-  data.requester = requester;
-  data.event = event;
-  $http.put('/event/approverequest/' + event._id, data).then(function(response){
-    console.log('got response from approveRequest PUT');
-    var index = ev.eventToView.pending.indexOf(requester);
-    ev.eventToView.pending.splice(index, 1);
-
-
-    ev.getMyEvents();
-  });
-};
-
-// admin approves pending request
-ev.denyRequest = function(eventId, requester){
-  console.log('in denyRequest');
-  console.log('requester is:',requester);
-  console.log('eventId is:', eventId);
-  $http.put('/event/denyrequest/' + eventId, {requester:requester}).then(function(response){
-    console.log('got response from approveRequest PUT');
-    var index = ev.eventToView.pending.indexOf(requester);
-    ev.eventToView.pending.splice(index, 1);
-
-    ev.getMyEvents();
-  });
-};
-
-ev.cancelAttend = function(eventId){
-  console.log('in cancelAttend with id:', eventId);
-
-  swal({
-    title: 'Are you sure?',
-    text: "It won't be the same without you.",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: "I'm not going!"
-  }).then(function () {
-    swal(
-      'You are freed of the burden of obligation.',
-      'You are no longer listed as attending.',
-
-      'success'
-    );
-    console.log('sending PUT to /event/removeattend/'+ eventId);
-    $http.put('/event/removeattend/' + eventId).then(function(response){
-      console.log('received response from cancelAttend PUT');
+  // Admin approves pending request
+  ev.approveRequest = function(event, requester){
+    console.log('in approveRequest');
+    console.log('requester is:',requester);
+    console.log('event is:', event);
+    var data = {};
+    data.requester = requester;
+    data.event = event;
+    $http.put('/event/approverequest/' + event._id, data).then(function(response){
+      console.log('got response from approveRequest PUT');
+      var index = ev.eventToView.pending.indexOf(requester);
+      ev.eventToView.pending.splice(index, 1);
       ev.getMyEvents();
     });
-  });
+  };
 
-
-};
-
-ev.cancelRequest = function(eventId){
-  console.log('in cancelRequest with id:', eventId);
-  swal({
-    title: 'Are you sure?',
-    text: "You can always re-request to attend.",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: "Yes, cancel my request!"
-  }).then(function () {
-    $http.put('/event/cancelrequest/' + eventId).then(function(response){
-      console.log('received response from cancelRequest PUT');
+  // Admin approves pending request
+  ev.denyRequest = function(eventId, requester){
+    console.log('in denyRequest');
+    console.log('requester is:',requester);
+    console.log('eventId is:', eventId);
+    $http.put('/event/denyrequest/' + eventId, {requester:requester}).then(function(response){
+      console.log('got response from approveRequest PUT');
+      var index = ev.eventToView.pending.indexOf(requester);
+      ev.eventToView.pending.splice(index, 1);
       ev.getMyEvents();
     });
-    swal(
-      'Done!',
-      'Your request to attend as been canceled.',
-      'success'
-    )
-  })
+  };
 
-};
+  // Remove user from list of Attending users
+  ev.cancelAttend = function(eventId){
+    console.log('in cancelAttend with id:', eventId);
 
-ev.removeFromSaved = function(eventId){
-  console.log('in removeFromSaved with id:', eventId);
-  $http.put('/event/removefromsaved/' + eventId).then(function(response){
-    console.log('received response from removeFromSaved PUT');
+    swal({
+      title: 'Are you sure?',
+      text: "It won't be the same without you.",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: "I'm not going!"
+    }).then(function () {
+      swal(
+        'You are freed of the burden of obligation.',
+        'You are no longer listed as attending.',
+        'success'
+      );
+      console.log('sending PUT to /event/removeattend/'+ eventId);
+      $http.put('/event/removeattend/' + eventId).then(function(response){
+        console.log('received response from cancelAttend PUT');
+        ev.getMyEvents();
+      });
+    });
+  };
 
-    ev.getMyEvents();
-  });
-};
+  // Cancel a request to attend an event
+  ev.cancelRequest = function(eventId){
+    console.log('in cancelRequest with id:', eventId);
+    swal({
+      title: 'Are you sure?',
+      text: "You can always re-request to attend.",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: "Yes, cancel my request!"
+    }).then(function () {
+      $http.put('/event/cancelrequest/' + eventId).then(function(response){
+        console.log('received response from cancelRequest PUT');
+        ev.getMyEvents();
+      });
+      swal(
+        'Done!',
+        'Your request to attend as been canceled.',
+        'success'
+      );
+    });
+  };
 
-ev.createEvent = function(){
-  console.log('creating event:', ev.newEvent);
-  $http.post('/event/createEvent', ev.newEvent).then(function(response){
+  // Remove event from My Events
+  ev.removeFromSaved = function(eventId){
+    console.log('in removeFromSaved with id:', eventId);
+    $http.put('/event/removefromsaved/' + eventId).then(function(response){
+      console.log('received response from removeFromSaved PUT');
+      ev.getMyEvents();
+    });
+  };
+
+  // Create a new event
+  ev.createEvent = function(){
+    console.log('creating event:', ev.newEvent);
+    $http.post('/event/createEvent', ev.newEvent).then(function(response){
       console.log('createEvent response:', response);
       swal(
-      'Event created!',
-      'Game on!',
-      'success'
-    );
-    $location.path('/myevents');
+        'Event created!',
+        'Game on!',
+        'success'
+      );
+      $location.path('/myevents');
       //if response is success, clear vm.newEvent and redirect to event page
-
     });
-};
+  };
 
-ev.deleteEvent = function(eventId){
-  console.log('in deleteEvent with id:', eventId);
-  swal({
-    title: 'Are you sure?',
-    text: "Deleting the event cannot be undone.",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: "Yes, delete my event!"
-  }).then(function () {
-    $http.delete('/event/deleteevent/' + eventId).then(function(response){
-      console.log('received response from deleteEvent REMOVE');
-      ev.getMyEvents();
+  // Delete an event
+  ev.deleteEvent = function(eventId){
+    console.log('in deleteEvent with id:', eventId);
+    swal({
+      title: 'Are you sure?',
+      text: "Deleting the event cannot be undone.",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: "Yes, delete my event!"
+    }).then(function () {
+      $http.delete('/event/deleteevent/' + eventId).then(function(response){
+        console.log('received response from deleteEvent REMOVE');
+        ev.getMyEvents();
+      });
+      swal(
+        'Done!',
+        'Your event has been deleted.',
+        'success'
+      );
     });
-    swal(
-      'Done!',
-      'Your event has been deleted.',
-      'success'
-    );
-  });
-};
+  };
 
-ev.getUserProfile = function(username){
-  console.log('in getUserProfile with username:', username);
-  $http.get('/user/userprofile/' + username).then(function(response){
-    console.log('received response from getUserProfile GET');
-    ev.userToView = response.data;
-    console.log('ev.userToView is:', ev.userToView);
-    $location.path('/userprofile');
+  // Gets a user to view then links to the view profile page
+  ev.getUserProfile = function(username){
+    console.log('in getUserProfile with username:', username);
+    $http.get('/user/userprofile/' + username).then(function(response){
+      console.log('received response from getUserProfile GET');
+      ev.userToView = response.data;
+      console.log('ev.userToView is:', ev.userToView);
+      $location.path('/userprofile');
+    });
+  };
 
-  });
+  // Sets color of events on My Events
+  ev.getColor = function(type){
+    console.log('in getColor');
+    if(type == "board"){
+      return "#e57737";
+    } else if(type == "outdoor"){
+      return "#39E589";
+    } else {
+      return "#6EC3E5";
+    }
+  };
 
-
-};
-
-ev.getColor = function(type){
-  console.log('in getColor');
-if(type == "board"){
-  return "#e57737";
-} else if(type == "outdoor"){
-  return "#39E589";
-} else {
-  return "#6EC3E5";
-}
-
-};
-
-
-ev.test = function(){
-  console.log('ev.eventToView.img:',ev.eventToView.img);
-
-};
-
-
-
-
-return ev;
+  return ev;
 
 });
--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -4,39 +4,33 @@ myApp.factory('UserService', function($http, $location){
   var userObject = {};
 
   return {
-
-
+      
     userObject : userObject,
 
     getuser : function(){
       console.log('UserService -- getuser');
       $http.get('/user').then(function(response) {
-          if(response.data.username) {
-              // user has a curret session on the server
-              userObject.userName = response.data.username;
-              userObject.email = response.data.email;
-              userObject.name = response.data.name;
-              userObject.location = response.data.location;
-              userObject.age = response.data.age;
-              userObject.gender = response.data.gender;
-              userObject.favoriteGames = response.data.favoriteGames;
-              userObject.contactInfo = response.data.contactInfo;
-              userObject.aboutMe = response.data.aboutMe;
-              userObject.img = response.data.img;
-              //userObject.blah = response.data.blah;
-              console.log('RESPONSE DOT DATA DOT IMAGE:', response.data.img);
-              console.log('USEROBJECT DOT IMAGE', userObject.img);
-
-
-
-
-              console.log('RESPONSE.DATA:', response.data);
-              console.log('UserService -- getuser -- User Data: ', userObject.userName);
-          } else {
-              console.log('UserService -- getuser -- failure');
-              // user has no session, bounce them back to the login page
-              $location.path("/home");
-          }
+        if(response.data.username) {
+          // user has a curret session on the server
+          userObject.userName = response.data.username;
+          userObject.email = response.data.email;
+          userObject.name = response.data.name;
+          userObject.location = response.data.location;
+          userObject.age = response.data.age;
+          userObject.gender = response.data.gender;
+          userObject.favoriteGames = response.data.favoriteGames;
+          userObject.contactInfo = response.data.contactInfo;
+          userObject.aboutMe = response.data.aboutMe;
+          userObject.img = response.data.img;
+          console.log('RESPONSE DOT DATA DOT IMAGE:', response.data.img);
+          console.log('USEROBJECT DOT IMAGE', userObject.img);
+          console.log('RESPONSE.DATA:', response.data);
+          console.log('UserService -- getuser -- User Data: ', userObject.userName);
+        } else {
+          console.log('UserService -- getuser -- failure');
+          // user has no session, bounce them back to the login page
+          $location.path("/home");
+        }
       },function(response){
         console.log('UserService -- getuser -- failure: ', response);
         $location.path("/home");
